afe9d7342e374ff85f518447bd3b15b380a44f23
mw <mw@mw>
2021-07-20 10:57:58 -0300

    Configuração do Hibernate com Postgre e Mysql (Comentado) -Classe Model - Pessoa (Mapeada) -Persistence.xml (Configurado e testado)

---
+++ meuprimeiroprojetojsf/src/test/java/br/com/meuprimeiroprojetojsf/TesteJPA.java
@@ -0,0 +1,13 @@
+package br.com.meuprimeiroprojetojsf;
+
+import javax.persistence.Persistence;
+
+public class TesteJPA {
+
+	public static void main(String[] args) {
+		
+		Persistence.createEntityManagerFactory("meuprimeiroprojetojsf");
+		
+	}
+	
+}
